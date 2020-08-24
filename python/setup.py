2e46b215f3f899c5f41edbaf02713b561bfc1a60
Raphael Dussin <Raphael.Dussin@noaa.gov>
2020-08-24 10:36:38 -0400

    packaging

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+''' setup for thinwalltopography '''
+import setuptools
+
+setuptools.setup(
+    name="thinwalltopography",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description=(""),
+    license="",
+    keywords="",
+    url="",
+    packages=['thinwalltopography']
+)
